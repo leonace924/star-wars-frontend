da57b309cabd3256b6f6a01106f19cf96e16c5aa
leonace924 <meobius123@gmail.com>
2021-11-27 04:32:07 -0500

    feat: add react router v6 and basic homepage

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-gray-600">
+      <button className="bg-purple-400">Test</button>
+    </div>
+  );
+};
+
+export default HomePage;
