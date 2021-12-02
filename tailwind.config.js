d381876706a17daa30e5f5040b684279be19b53c
leonace924 <meobius123@gmail.com>
2021-12-02 13:14:42 -0500

    feat: enable tailwind css live reload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  mode: "jit",
+  mode: 'jit',
   purge: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/**/*.{js,ts,jsx,tsx}",
-    "./public/index.html",
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './public/index.html',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outline: ['"Londrina Outline"', '"Georgia"', '"Times New Roman"', 'serif'],
+      },
+    },
   },
   variants: {
     extend: {},
