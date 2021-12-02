cfe1d1a4ce3e9123410421f90fbff5b424fc438c
leonace924 <meobius123@gmail.com>
2021-12-02 14:27:58 -0500

    feat: update people card to redirect detail page

--- src/components/peopleCard.js
+++ src/components/peopleCard.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useGlobal } from 'stores/global-store';
 
 const PeopleCard = ({ people }) => {
-  console.log(people);
+  let navigate = useNavigate();
+
+  const setSelectPeople = useGlobal((state) => state.setSelectPeople);
+
+  const slug = people.name.split(' ').join('-').toLowerCase();
+
+  const handleRedirect = () => {
+    setSelectPeople(people);
+    navigate(`/${slug}`);
+  };
   return (
     <div className="p-6 bg-white rounded-lg shadow-lg">
       <div className="flex items-baseline">
@@ -15,9 +26,11 @@ const PeopleCard = ({ people }) => {
       </h4>
 
       <div className="mt-4">
-        <span className="font-semibold text-gray-800 underline transition-all duration-300 cursor-pointer text-md hover:no-underline hover:text-star-danger">
-          View Detail
-        </span>
+        <div onClick={handleRedirect}>
+          <span className="font-semibold text-gray-800 underline transition-all duration-300 cursor-pointer text-md hover:no-underline hover:text-star-danger">
+            View Detail
+          </span>
+        </div>
       </div>
     </div>
   );
